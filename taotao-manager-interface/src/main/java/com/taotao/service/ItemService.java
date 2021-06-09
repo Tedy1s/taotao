adb19bcdf11500f854f88772c22333edc4498452
Tedy1s <gongpin020405875@126.com>
2021-06-09 23:26:43 +0800

    添加各种配置文件

---
+++ taotao-manager-interface/src/main/java/com/taotao/service/ItemService.java
@@ -0,0 +1,9 @@
+package com.taotao.service;
+
+/**
+ * function:
+ * date:       2018/2/24 0024上午 09:34
+ * author:     王小羊
+ */
+public interface ItemService {
+}
---
+++ taotao-manager-service/src/main/java/com/taotao/service/impl/ItemServiceImpl.java
@@ -0,0 +1,16 @@
+package com.taotao.service.impl;
+
+import com.taotao.service.ItemService;
+import org.springframework.stereotype.Service;
+
+
+/**
+ * function:
+ * date:       2018/2/24 0024上午 09:36
+ * author:     王小羊
+ */
+
+@Service
+public class ItemServiceImpl implements ItemService{
+
+}
